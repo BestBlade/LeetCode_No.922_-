7c1dbcd55a9ed59ccf4c1047c6da7fa315a2cd37
BestBlade <68151785+BestBlade@users.noreply.github.com>
2020-11-12 10:04:46 +0800

    Add files via upload

---
+++ leetcode_no922_按奇偶排序数组II_w1.cpp
@@ -0,0 +1,66 @@
+﻿/* ------------------------------------------------------------------|
+给定一个非负整数数组 A， A 中一半整数是奇数，一半整数是偶数。
+
+对数组进行排序，以便当 A[i] 为奇数时，i 也是奇数；当 A[i] 为偶数时， i 也是偶数。
+
+你可以返回任何满足上述条件的数组作为答案。
+
+ 
+
+示例：
+
+输入：[4,2,5,7]
+输出：[4,5,2,7]
+解释：[4,7,2,5]，[2,5,4,7]，[2,7,4,5] 也会被接受。
+ 
+
+提示：
+
+2 <= A.length <= 20000
+A.length % 2 == 0
+0 <= A[i] <= 1000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sort-array-by-parity-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	双指针法
+*
+*	执行用时：32 ms, 在所有 C++ 提交中击败了99.18%的用户
+*	内存消耗：20.4 MB, 在所有 C++ 提交中击败了32.52%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+vector<int> sortArrayByParityII(vector<int>& A) {
+    int even = 0;
+    int odd = 1;
+
+    int len = A.size();
+
+    for (; even < len; even += 2) {
+        if (A[even] % 2) {
+            while (A[odd] % 2) {
+                odd += 2;
+            }
+            swap(A[odd], A[even]);
+        }
+    }
+    return A;
+}
